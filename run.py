3c5c118a88e79326e9470c361ff16959be3d5183
Andy Mao <andy@unraveldata.com>
2018-12-12 15:57:57 -0800

    fix typo

--- run.py
+++ run.py
@@ -24,7 +24,6 @@ def auto_update():
 
 def update_confluence(atlassian_base_url):
     unravel_base_url = 'http://localhost:3000'
-    atlassian_base_url = 'https://unraveldata.atlassian.net/wiki/rest/api/content/284262500'
     # atlassian_base_url2 = 'https://unraveldata.atlassian.net/wiki/rest/api/content/502628605'
     credentials = b64e('%s:%s' % (argv.username, argv.password))
 
